9a2f6f0fa7ba38c343922138a7f5b0b5fb308cf1
VandevilleF <flo.vandeville@gmail.com>
2024-04-22 10:47:00 +0200

    Remove math imort and change type of return float by int

--- python_variable_annotations/2-floor.py
+++ python_variable_annotations/2-floor.py
@@ -1,9 +1,8 @@
 #!/usr/bin/env python3
 """A type-annotated function floor"""
-import math
 
 
-def floor(n: float) -> float:
+def floor(n: float) -> int:
     """A type-annotated function floor which takes
     a float n as argument and returns the floor of the float."""
-    return math.floor(n)
+    return int(n)
