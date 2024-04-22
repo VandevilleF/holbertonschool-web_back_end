49cb5fd983a631d6938932ed4570931cf6e297cc
VandevilleF <flo.vandeville@gmail.com>
2024-04-22 10:29:33 +0200

    Add file for task 1, local test OK

---
+++ python_variable_annotations/1-concat.py
@@ -0,0 +1,8 @@
+#!/usr/bin/env python3
+"""A type-annotated function concat"""
+
+
+def concat(str1: str, str2: str) -> str:
+    """A type-annotated func concat that takes a string str1
+    and a string str2 as arg and returns a concatenated string"""
+    return (str1 + str2)
