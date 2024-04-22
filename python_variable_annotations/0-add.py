ffc393695a3c1e37c431037aefc593e537c4a528
VandevilleF <flo.vandeville@gmail.com>
2024-04-22 10:15:16 +0200

    Add file for task 0, local test OK

---
+++ python_variable_annotations/0-add.py
@@ -0,0 +1,8 @@
+#!/usr/bin/env python3
+"""A type-annotated function add"""
+
+
+def add(a: float, b: float) -> float:
+    """A type-annotated func add that takes a float a and a float b
+    as arguments and returns their sum as a float"""
+    return (a + b)
