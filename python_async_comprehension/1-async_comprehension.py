456a1f478a633c09f387a4d65b57383af2557827
VandevilleF <flo.vandeville@gmail.com>
2024-04-23 14:52:01 +0200

    Change syntaxe construction

--- python_async_comprehension/1-async_comprehension.py
+++ python_async_comprehension/1-async_comprehension.py
@@ -7,7 +7,5 @@ async_generator = __import__('0-async_generator').async_generator
 async def async_comprehension() -> List[float]:
     """collect 10 random numbers using an async comprehensing
     over async_generator, then return the 10 random numbers."""
-    collect_random = []
-    async for i in async_generator():
-        collect_random.append(i)
+    collect_random = [rd_nb async for rd_nb in async_generator()]
     return collect_random
