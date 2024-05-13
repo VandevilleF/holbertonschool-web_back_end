f7708479b2244db277b7d45a54214efbf0338ace
VandevilleF <flo.vandeville@gmail.com>
2024-05-13 15:35:35 +0200

    Refact code and use .finally

--- ES6_promise/2-then.js
+++ ES6_promise/2-then.js
@@ -1,9 +1,6 @@
 export default function handleResponseFromAPI(promise) {
-  promise.then(function() {
-    console.log('Got a response from the API');
-    return { status: 200, body: 'Success' };
-  }).catch(function() {
-    console.log('Got a response from the API');
-    return new Error();
-  });
+  return promise
+    .then(() => ({ status: 200, body: 'Success' }))
+    .catch(() => new Error(''))
+    .finally(() => console.log('Got a response from the API'));
 }
