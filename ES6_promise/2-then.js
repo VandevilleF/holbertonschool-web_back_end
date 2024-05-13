12299112a13e782013d429618eada5ae42e20fd0
VandevilleF <flo.vandeville@gmail.com>
2024-05-13 15:29:21 +0200

    Add file for task 2, local test OK

---
+++ ES6_promise/2-then.js
@@ -0,0 +1,9 @@
+export default function handleResponseFromAPI(promise) {
+  promise.then(function() {
+    console.log('Got a response from the API');
+    return { status: 200, body: 'Success' };
+  }).catch(function() {
+    console.log('Got a response from the API');
+    return new Error();
+  });
+}
