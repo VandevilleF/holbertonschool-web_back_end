8fedb316ee59d006075949c57b8df916fbf05a8b
VandevilleF <flo.vandeville@gmail.com>
2024-05-13 15:42:29 +0200

    whyyyy ???

--- ES6_promise/2-then.js
+++ ES6_promise/2-then.js
@@ -1,6 +1,7 @@
 export default function handleResponseFromAPI(promise) {
-  return promise
-    .then(() => ({ status: 200, body: 'Success' }))
-    .catch(() => Error())
-    .finally(() => console.log('Got a response from the API'));
+  return promise.then(() => {
+    console.log('Got a response from the API');
+    return { status: 200, body: 'success' };
+  })
+    .catch(() => new Error());
 }
