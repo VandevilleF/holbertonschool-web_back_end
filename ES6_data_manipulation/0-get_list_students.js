549e801a23438cd28fa0d75f0d09cb3be4f6f961
VandevilleF <flo.vandeville@gmail.com>
2024-05-20 11:16:18 +0200

    test

--- ES6_data_manipulation/0-get_list_students.js
+++ ES6_data_manipulation/0-get_list_students.js
@@ -1,6 +1,13 @@
-export default function getListStudents () {
-  return [{ id: 1, firstName: 'Guillaume', location: 'San Francisco' },
+export default function getListStudents() {
+  const listStudents = [
+    { id: 1, firstName: 'Guillaume', location: 'San Francisco' },
     { id: 2, firstName: 'James', location: 'Columbia' },
-    { id: 5, firstName: 'Serena', location: 'San Francisco' }
+    { id: 5, firstName: 'Serena', location: 'San Francisco' },
   ];
+
+  return listStudents.map((student) => ({
+    id: student.id,
+    firstName: student.firstName,
+    location: student.location,
+  }));
 }
