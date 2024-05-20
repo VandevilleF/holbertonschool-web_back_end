6ef63abbb7726df52021b337adb5560979334519
VandevilleF <flo.vandeville@gmail.com>
2024-05-20 12:44:39 +0200

    Add space after and before "{...}"

--- ES6_data_manipulation/4-update_grade_by_city.js
+++ ES6_data_manipulation/4-update_grade_by_city.js
@@ -2,6 +2,6 @@ export default function updateStudentGradeByCity(students, city, newGrades) {
   return students.filter((student) => student.location === city)
   .map((student) => {
     const SGrade = newGrades.find((grade) => grade.studentId === student.id);
-    return {...student, grade: SGrade ? SGrade.grade : 'N/A',};
+    return { ...student, grade: SGrade ? SGrade.grade : 'N/A', };
   });
 }
