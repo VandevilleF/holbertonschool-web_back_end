e756c1956ae61cac30a22965f4898a1e80fde4ba
VandevilleF <flo.vandeville@gmail.com>
2024-05-20 13:42:44 +0200

    Checker de merde la !!!

--- ES6_data_manipulation/5-typed_arrays.js
+++ ES6_data_manipulation/5-typed_arrays.js
@@ -1,6 +1,6 @@
 export default function createInt8TypedArray(length, position, value) {
   if (position >= length) {
-    throw error('Position outside range')
+    throw Error('Position outside range');
   }
   const buffer = new ArrayBuffer(length);
   const int8 = new DataView(buffer);
