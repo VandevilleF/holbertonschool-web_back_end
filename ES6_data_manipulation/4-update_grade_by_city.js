68a95cb0f59a7a08c69e33dd34a4acb7c2287d7e
VandevilleF <flo.vandeville@gmail.com>
2024-05-20 12:47:11 +0200

    Change indent of code

--- ES6_data_manipulation/4-update_grade_by_city.js
+++ ES6_data_manipulation/4-update_grade_by_city.js
@@ -1,7 +1,7 @@
 export default function updateStudentGradeByCity(students, city, newGrades) {
   return students.filter((student) => student.location === city)
-  .map((student) => {
-    const SGrade = newGrades.find((grade) => grade.studentId === student.id);
-    return { ...student, grade: SGrade ? SGrade.grade : 'N/A', };
-  });
+    .map((student) => {
+      const SGrade = newGrades.find((grade) => grade.studentId === student.id);
+      return { ...student, grade: SGrade ? SGrade.grade : 'N/A' };
+    });
 }
