69334bf32c6885c0e688aa1b63130f9a7cbb5eaf
VandevilleF <flo.vandeville@gmail.com>
2024-05-20 13:38:33 +0200

    Change line 6 for DataView view

--- ES6_data_manipulation/5-typed_arrays.js
+++ ES6_data_manipulation/5-typed_arrays.js
@@ -3,7 +3,7 @@ export default function createInt8TypedArray(length, position, value) {
     throw error('Position outside range')
   }
   const buffer = new ArrayBuffer(length);
-  const int8 = new Int8Array(buffer);
-  int8[position] = value;
-  return new DataView(buffer);
+  const int8 = new DataView(buffer);
+  int8.setInt8(position, value)
+  return int8;
 }
