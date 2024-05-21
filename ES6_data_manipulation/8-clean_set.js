54e455fd1c03a16e7e5ef73f917e8eba781af833
VandevilleF <flo.vandeville@gmail.com>
2024-05-21 10:21:53 +0200

    Add file for task 8, local test OK

---
+++ ES6_data_manipulation/8-clean_set.js
@@ -0,0 +1,12 @@
+export default function cleanSet(set, startString) {
+  if (startString === '') {
+    return '';
+  }
+  const result = [];
+  set.forEach(element => {
+    if ( element.startsWith(startString)) {
+      result.push(element.slice(startString.length));
+    }
+  });
+  return result.join('-');
+}
