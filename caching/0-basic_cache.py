81c6906d46b18c4331e0e1d38cc46bcb0ac0a286
VandevilleF <flo.vandeville@gmail.com>
2025-01-27 13:19:18 +0100

    comparisons "==" to "is" and add blank line between methods of class

--- caching/0-basic_cache.py
+++ caching/0-basic_cache.py
@@ -7,19 +7,21 @@ class BasicCache(BaseCaching):
     """Child class inherit of class BaseCaching that defines:
     - constants of your caching system
     - where your data are stored (in a dictionary)"""
+
     def __init__(self):
         """Initiliaze"""
         BaseCaching.__init__(self)
+
     def put(self, key, item):
         """Add an item in the cache"""
-        if (key == None or item == None):
+        if (key is None or item is None):
             pass
         else:
             self.cache_data[key] = item
 
     def get(self, key):
         """Get an item by key"""
-        if (key == None or self.cache_data.get(key) == None):
+        if (key is None or self.cache_data.get(key) is None):
             return
         else:
             return self.cache_data[key]
