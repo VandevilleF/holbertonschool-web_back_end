1dc131aa760cbb91eea6b8f3d0c8bb22aa7366c1
VandevilleF <flo.vandeville@gmail.com>
2025-02-10 12:22:54 +0100

    refact function to use a single regex

--- personal_data/filtered_logger.py
+++ personal_data/filtered_logger.py
@@ -4,16 +4,6 @@ import re
 
 
 def filter_datum(fields, redaction, message, separator):
-    """Function that returns the log message obfuscated
-    Args:
-        field: a list of strings representing all
-        fields to obfuscate
-        redaction: a string representing by
-        what the field will be obfuscated
-        message: a string representing the log line
-        separator: a string representing by which character
-        is separating all fields in the log line (message)"""
-    for field in fields:
-        message = re.sub(rf'{field}=.*?{separator}',
-                         f'{field}={redaction}{separator}', message)
-    return message
+    """Return the log message obfuscated"""
+    return re.sub(rf'({"|".join(fields)})=[^ {separator}]*',
+                  rf'\1={redaction}', message)
