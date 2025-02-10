bfc7a267f2c769989fc2c07ec0bffa3d5477b619
VandevilleF <flo.vandeville@gmail.com>
2025-02-10 12:36:00 +0100

    Whyy ?

--- personal_data/filtered_logger.py
+++ personal_data/filtered_logger.py
@@ -1,11 +1,13 @@
 #!/usr/bin/env python3
-"""filtered_logger module"""
+"""
+filtered_logger module.
+"""
 import re
 
 
 def filter_datum(fields, redaction, message, separator):
     """
-    Returns the log message obfuscated by replacing field values with a redacted string.
+    Returns the log message obfuscated.
     """
-    pattern = f'({"|".join(fields)})=[^\{separator}]*'
-    return re.sub(pattern, lambda m: f"{m.group(1)}={redaction}", message)
+    pattern = f"({'|'.join(fields)})=[^{separator}]*"
+    return re.sub(pattern, f'\\1={redaction}', message)
