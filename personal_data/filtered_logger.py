56ec12c67bc51b5c89285dd4095901aba94f6255
VandevilleF <flo.vandeville@gmail.com>
2025-02-10 12:39:53 +0100

    bakcup

--- personal_data/filtered_logger.py
+++ personal_data/filtered_logger.py
@@ -1,6 +1,13 @@
 #!/usr/bin/env python3
+"""
+filtered_logger module.
+"""
 import re
 
+
 def filter_datum(fields, redaction, message, separator):
+    """
+    Returns the log message obfuscated.
+    """
     pattern = f"({'|'.join(fields)})=[^{separator}]*"
-    return re.sub(pattern, f"\\1={redaction}", message)
+    return re.sub(pattern, f'\\1={redaction}', message)
