7dc052704abbeaf572067175cdf1d38cababd4d3
VandevilleF <flo.vandeville@gmail.com>
2025-02-12 14:37:49 +0100

    Task 8, local test OK

--- Basic_authentication/api/v1/auth/basic_auth.py
+++ Basic_authentication/api/v1/auth/basic_auth.py
@@ -4,6 +4,7 @@ Basic_auth module
 """
 
 from api.v1.auth.auth import Auth
+import base64
 
 
 class BasicAuth(Auth):
@@ -23,3 +24,21 @@ class BasicAuth(Auth):
             return authorization_header.partition('Basic ')[2]
 
         return None
+
+    def decode_base64_authorization_header(self,
+                                           base64_authorization_header:
+                                               str) -> str:
+        """ Return:
+        - the decoded value of a Base64 string"""
+        if base64_authorization_header is None:
+            return None
+        if type(base64_authorization_header) is not str:
+            return None
+
+        try:
+            decode = base64.b64decode(
+                base64_authorization_header).decode('utf-8')
+            return decode
+
+        except (TypeError, ValueError):
+            return None
