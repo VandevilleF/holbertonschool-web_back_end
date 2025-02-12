b51f95d82ace59b02c171f734a6ca04868e41c41
VandevilleF <flo.vandeville@gmail.com>
2025-02-12 13:05:25 +0100

    Task 6, local test OK

--- Basic_authentication/api/v1/auth/basic_auth.py
+++ Basic_authentication/api/v1/auth/basic_auth.py
@@ -9,4 +9,17 @@ from api.v1.auth.auth import Auth
 class BasicAuth(Auth):
     """ BasicAuth
     """
-    pass
+    def extract_base64_authorization_header(self,
+                                            authorization_header: str) -> str:
+        """ Return:
+        - the Base64 part of the Authorization header
+        """
+        if authorization_header is None:
+            return None
+        if type(authorization_header) is not str:
+            return None
+
+        if authorization_header.startswith('Basic '):
+            return authorization_header.partition('Basic ')[2]
+
+        return None
