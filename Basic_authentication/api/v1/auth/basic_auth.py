401ae4fc2ecd47d02d144271df432f7b5230deb7
VandevilleF <flo.vandeville@gmail.com>
2025-02-12 15:48:02 +0100

    Task 9, local test OK

--- Basic_authentication/api/v1/auth/basic_auth.py
+++ Basic_authentication/api/v1/auth/basic_auth.py
@@ -42,3 +42,19 @@ class BasicAuth(Auth):
 
         except (TypeError, ValueError):
             return None
+
+    def extract_user_credentials(self,
+                                 decoded_base64_authorization_header: str) -> (str, str):
+        """ Return:
+        the user email and password from the Base64 decoded value.
+        """
+        if decoded_base64_authorization_header is None:
+            return None, None
+        if type(decoded_base64_authorization_header) is not str:
+            return None, None
+        if ':' not in decoded_base64_authorization_header:
+            return None, None
+
+        email, _, password = decoded_base64_authorization_header.partition(':')
+
+        return email, password
