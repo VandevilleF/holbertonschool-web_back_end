bf94b830f45578ac2388a2a9ab10df8987cf51d3
VandevilleF <flo.vandeville@gmail.com>
2025-02-12 16:43:21 +0100

    Task 10, local test OK and new line in gitignore for json file

--- Basic_authentication/api/v1/auth/basic_auth.py
+++ Basic_authentication/api/v1/auth/basic_auth.py
@@ -5,6 +5,8 @@ Basic_auth module
 
 from api.v1.auth.auth import Auth
 import base64
+from models.user import User
+from typing import TypeVar
 
 
 class BasicAuth(Auth):
@@ -59,3 +61,24 @@ class BasicAuth(Auth):
         email, _, password = decoded_base64_authorization_header.partition(':')
 
         return email, password
+
+    def user_object_from_credentials(
+        self, user_email: str, user_pwd: str
+    ) -> TypeVar('User'):
+        """ Return:
+        the User instance based on his email and password
+        """
+        if user_email is None or type(user_email) is not str:
+            return None
+        if user_pwd is None or type(user_pwd) is not str:
+            return None
+
+        users = User.search({"email": user_email})
+        if not users:
+            return None
+
+        user = users[0]
+        if not user.is_valid_password(user_pwd):
+            return None
+
+        return user
