e04d2b82de502e1d4f7d9424dc6ee60fb3630b69
VandevilleF <flo.vandeville@gmail.com>
2025-02-13 19:00:38 +0100

    Task 7, local test OK

--- Session_authentication/api/v1/views/users.py
+++ Session_authentication/api/v1/views/users.py
@@ -30,7 +30,7 @@ def view_one_user(user_id: str = None) -> str:
     if user_id == "me":
         if request.current_user is None:
             abort(404)
-        print("user", type(request.current_user), request.current_user)
+
         user = request.current_user
     else:
         user = User.get(user_id)
