7eb1b16786ddbfdd45880f4ce4484b78d8ee3fc3
VandevilleF <flo.vandeville@gmail.com>
2025-02-13 19:27:59 +0100

    Task 8, local test OK

--- Session_authentication/api/v1/views/session_auth.py
+++ Session_authentication/api/v1/views/session_auth.py
@@ -37,3 +37,17 @@ def login():
     response.set_cookie(session_name, session_id)
 
     return response
+
+
+@app_views.route('/auth_session/logout',
+                 methods=['DELETE'],
+                 strict_slashes=False)
+def delete():
+    """ Deletes the user session / logout
+    """
+    from api.v1.app import auth
+
+    if not auth.destroy_session(request):
+        abort(404)
+
+    return jsonify({}), 200
