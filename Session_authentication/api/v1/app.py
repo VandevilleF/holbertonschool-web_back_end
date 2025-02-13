f009a5c66ec46f9825a0d7b03c627ce2633f34a9
VandevilleF <flo.vandeville@gmail.com>
2025-02-13 14:59:11 +0100

    Task 0, local test OK

--- Session_authentication/api/v1/app.py
+++ Session_authentication/api/v1/app.py
@@ -33,6 +33,10 @@ def before_request():
         return
     if (auth.authorization_header(request) is None):
         abort(401)
+
+    request.current_user = auth.current_user(request)
+    print("app", type(request.current_user), request.current_user)
+
     if (auth.current_user(request) is None):
         abort(403)
 
