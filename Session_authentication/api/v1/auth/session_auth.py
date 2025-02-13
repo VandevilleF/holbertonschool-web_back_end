93c73da80d7c3df9f141928afe88177ae14e33d5
VandevilleF <flo.vandeville@gmail.com>
2025-02-13 18:04:51 +0100

    Test task 6

--- Session_authentication/api/v1/auth/session_auth.py
+++ Session_authentication/api/v1/auth/session_auth.py
@@ -3,6 +3,7 @@
 SessionAuth module
 """
 from api.v1.auth.auth import Auth
+from api.v1.views.users import User
 import uuid
 
 
@@ -31,3 +32,26 @@ class SessionAuth(Auth):
             return None
 
         return self.user_id_by_session_id.get(session_id)
+
+    def current_user(self, request=None):
+        """ Return:
+        - a User instance based on a cookie value
+        """
+        if request is None:
+            return None
+
+        session_id = self.session_cookie(request)
+
+        if session_id is None:
+            return None
+
+        user_id = self.user_id_for_session_id(session_id)
+        print("User ID trouvé:", user_id)
+
+        if user_id is None:
+            return None
+
+        user = User.get(user_id)
+        print("Utilisateur trouvé:", user)
+
+        return user
