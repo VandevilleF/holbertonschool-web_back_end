86311d3f8011d778bac202b9ab960be68936443b
VandevilleF <flo.vandeville@gmail.com>
2025-02-24 18:00:05 +0100

    Task 7, local test OK

--- user_authentication_service/app.py
+++ user_authentication_service/app.py
@@ -1,9 +1,11 @@
 #!/usr/bin/env python3
 """Setup basic Flask
 """
-from flask import Flask, jsonify
+from flask import Flask, jsonify, request
+from auth import Auth
 
 
+AUTH = Auth()
 app = Flask(__name__)
 
 
@@ -12,6 +14,23 @@ def welcome():
     """Display a welcome message
     """
     return jsonify({"message": "Bienvenue"}), 200
+
+
+@app.route("/users", methods=["POST"], strict_slashes=False)
+def users():
+    """Registre a user
+    """
+    email = request.form.get('email')
+    pwd = request.form.get('password')
+
+    if not email or not pwd:
+        return jsonify({"message": "email and password are required"}), 400
+
+    try:
+        AUTH.register_user(email, pwd)
+        return jsonify({"email": email, "message": "user created"}), 201
+    except ValueError:
+        return jsonify({"message": "email already registered"}), 400
 
 
 if __name__ == "__main__":
