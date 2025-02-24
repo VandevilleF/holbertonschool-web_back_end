2de2e9913785cd3352a74967cc60b5c92b792b04
VandevilleF <flo.vandeville@gmail.com>
2025-02-24 12:43:01 +0100

    Task 2, local test OK

--- user_authentication_service/db.py
+++ user_authentication_service/db.py
@@ -5,6 +5,8 @@ from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
 from sqlalchemy.orm.session import Session
+from sqlalchemy.exc import InvalidRequestError
+from sqlalchemy.orm.exc import NoResultFound
 
 from user import Base, User
 
@@ -38,3 +40,19 @@ class DB:
         self._session.commit()
 
         return user
+
+    def find_user_by(self, **kwargs):
+        """Rreturns the first row found in the users table
+        as filtered by the method’s input arguments.
+        """
+        try:
+            user = self._session.query(User).filter_by(**kwargs).first()
+
+            if user is None:
+                raise NoResultFound
+            return user
+        
+        except NoResultFound as e:
+            raise e
+        except InvalidRequestError as e:
+            raise e
