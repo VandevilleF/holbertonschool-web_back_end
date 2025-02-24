2eb9861d92e084d9d52941f4c2f609adb52095e4
VandevilleF <flo.vandeville@gmail.com>
2025-02-24 16:24:13 +0100

    Task 4, local test OK

--- user_authentication_service/auth.py
+++ user_authentication_service/auth.py
@@ -7,6 +7,4 @@ from bcrypt import hashpw, gensalt
 def _hash_password(password: str) -> bytes:
     """Returns bytes
     """
-    hash = hashpw(password.encode('utf-8'), gensalt())
-    print(type(hash))
-    return hash
+    return hashpw(password.encode('utf-8'), gensalt())
