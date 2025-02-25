bcd7e244e10e2d490f8fc2a701e3b9ddb45ebf4b
VandevilleF <flo.vandeville@gmail.com>
2025-02-25 16:32:50 +0100

    check if cookie and user is None

--- user_authentication_service/app.py
+++ user_authentication_service/app.py
@@ -76,11 +76,11 @@ def logout() -> None:
       - If the user does not exist, 403 status
     """
     session_id = request.cookies.get('session_id')
-    if not session_id:
+    if session_id is None:
         abort(403)
 
     user = AUTH.get_user_from_session_id(session_id)
-    if not user:
+    if user is None:
         abort(403)
 
     AUTH.destroy_session(user.id)
