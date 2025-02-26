4a3fdcb19f723c4b7f9c2d2e8853c394b6289195
VandevilleF <flo.vandeville@gmail.com>
2025-02-26 10:24:48 +0100

    Task 16, local test OK

--- user_authentication_service/auth.py
+++ user_authentication_service/auth.py
@@ -79,10 +79,27 @@ class Auth:
         """
         try:
             user = self._db.find_user_by(id=user_id)
-            user.session_id = None
+            self._db.update_user(user.id, sesion_id=None)
             self._db._session.commit()
 
             return None
 
         except NoResultFound:
             return None
+
+    def get_reset_password_token(self, email: str) -> str:
+        """ Find the user corresponding to the email
+        Returns:
+            str: the new token
+            raise ValueError if the user does not exist
+        """
+        try:
+            user = self._db.find_user_by(email=email)
+
+        except NoResultFound:
+            raise ValueError
+
+        token = _generate_uuid()
+        self._db.update_user(user.id, reset_token=token)
+
+        return token
