15729aa963efa8d4775ed9a6f5b4ccfeaef7a3ce
VandevilleF <flo.vandeville@gmail.com>
2025-03-04 23:18:30 +0100

    Task 8, local test OK

--- Unittests_and_integration_tests/test_client.py
+++ Unittests_and_integration_tests/test_client.py
@@ -3,9 +3,8 @@
 """
 import unittest
 from client import GithubOrgClient
-from utils import get_json
-from unittest.mock import patch, PropertyMock
-from parameterized import parameterized
+from unittest.mock import patch, PropertyMock, Mock
+from parameterized import parameterized, parameterized_class
 
 
 class TestGithubOrgClient(unittest.TestCase):
@@ -65,3 +64,71 @@ class TestGithubOrgClient(unittest.TestCase):
         """
         self.assertEqual(GithubOrgClient.has_license(repo, license_key),
                          expected)
+
+
+org_payload = {
+    "login": "google",
+    "id": 1342004,
+    "node_id": "MDEyOk9yZ2FuaXphdGlvbjEzNDIwMDQ=",
+    "url": "https://api.github.com/orgs/google",
+    "repos_url": "https://api.github.com/orgs/google/repos",
+}
+
+repos_payload = [
+    {
+        "id": 1,
+        "name": "repo1",
+        "full_name": "google/repo1",
+        "license": {"key": "apache-2.0"}
+    },
+    {
+        "id": 2,
+        "name": "repo2",
+        "full_name": "google/repo2",
+        "license": {"key": "mit"}
+    }
+]
+
+expected_repos = ["repo1", "repo2"]
+apache2_repos = ["repo1"]
+
+
+@parameterized_class([
+    {"org_payload": org_payload,
+     "repos_payload": repos_payload,
+     "expected_repos": expected_repos,
+     "apache2_repos": apache2_repos}
+])
+class TestIntegrationGithubOrgClient(unittest.TestCase):
+    """Integration tests for GithubOrgClient methods
+    """
+
+    @classmethod
+    def setUpClass(cls):
+        """Set up class method to patch requests.get"""
+        cls.get_patcher = patch('requests.get')
+        mock_get = cls.get_patcher.start()
+
+        # Configure the mock to return the appropriate fixture
+        mock_get.side_effect = cls.get_side_effect
+
+    @classmethod
+    def tearDownClass(cls):
+        """Tear down class method to stop patcher"""
+        cls.get_patcher.stop()
+
+    @staticmethod
+    def get_side_effect(url):
+        """Side effect method to return the appropriate fixture based on the
+        URL"""
+        if url == "https://api.github.com/orgs/google":
+            return Mock(json=lambda: org_payload)
+        if url == "https://api.github.com/orgs/google/repos":
+            return Mock(json=lambda: repos_payload)
+        return Mock(json=lambda: {})
+
+    def test_public_repos(self):
+        """Test the public_repos method"""
+        client = GithubOrgClient("google")
+        self.assertEqual(client.public_repos(), self.expected_repos)
+        self.assertEqual(client.public_repos("apache-2.0"), self.apache2_repos)
