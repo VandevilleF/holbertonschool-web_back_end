0d68344c0264d904ed3f5d281af7e838cb430ff2
VandevilleF <flo.vandeville@gmail.com>
2025-03-17 19:12:57 +0100

    Task 1, local test OK

---
+++ MySQL_Advanced/1-country_users.sql
@@ -0,0 +1,9 @@
+-- script that creates a table users
+--
+CREATE TABLE IF NOT EXISTS users (
+	id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
+	email VARCHAR(255) NOT NULL UNIQUE,
+	name VARCHAR(255),
+	country VARCHAR(2) DEFAULT 'US',
+	CHECK (country IN ('US', 'CO', 'TN'))
+);
