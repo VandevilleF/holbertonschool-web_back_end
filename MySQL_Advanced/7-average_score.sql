ad34f62f429f560f9967632a3c2d0732980e816a
VandevilleF <flo.vandeville@gmail.com>
2025-03-19 16:58:14 +0100

    Back up for the process to count the average and create an alias for user_id in correction to ensure conflict

--- MySQL_Advanced/7-average_score.sql
+++ MySQL_Advanced/7-average_score.sql
@@ -1,18 +1,18 @@
 -- script that creates a stored procedure ComputeAverageScoreForUser that computes and store the average score for a student.
 --
+DROP PROCEDURE IF EXISTS ComputeAverageScoreForUser;
 DELIMITER //
 
 CREATE PROCEDURE ComputeAverageScoreForUser (IN user_id INT)
 BEGIN
-DECLARE total_score FLOAT;
-DECLARE total_corrections INT;
+DECLARE average_user FLOAT;
 
-SELECT SUM(score), COUNT(*) INTO total_score, total_corrections
-FROM corrections
-WHERE user_id = user_id;
+SELECT AVG(score) INTO average_user
+FROM corrections AS c
+WHERE c.user_id = user_id;
 
 UPDATE users
-SET average_score = (total_score / total_corrections)
+SET average_score = average_user
 WHERE id = user_id;
 
 END;
