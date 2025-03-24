fe993d4393ee32915f71a146dc26463a20f1528f
VandevilleF <flo.vandeville@gmail.com>
2025-03-24 12:07:12 +0100

    remove repetition in file test

--- unittests_in_js/0-calcul.test.js
+++ unittests_in_js/0-calcul.test.js
@@ -5,18 +5,15 @@ describe("calculateNumber", function() {
   it("Round a and b and return the sum of it", function() {
     assert.equal(calculateNumber(1, 3), 4);
   });
-});
-describe("calculateNumber", function() {
+
   it("Round a and b and return the sum of it", function() {
     assert.equal(calculateNumber(1, 3.7), 5);
   });
-});
-describe("calculateNumber", function() {
+
   it("Round a and b and return the sum of it", function() {
     assert.equal(calculateNumber(1.2, 3.7), 5);
   });
-});
-describe("calculateNumber", function() {
+
   it("Round a and b and return the sum of it", function() {
     assert.equal(calculateNumber(1.5, 3.7), 6);
   });
