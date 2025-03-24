15685b578cc2ed870daa1f11b08fb61b49f7c50e
VandevilleF <flo.vandeville@gmail.com>
2025-03-24 21:52:05 +0100

    Handle error in function and in the test

--- unittests_in_js/6-payment_token.js
+++ unittests_in_js/6-payment_token.js
@@ -2,6 +2,7 @@ function getPaymentTokenFromAPI(success) {
   if (success) {
     return Promise.resolve({data: 'Successful response from the API' })
   }
+  return Promise.reject(new Error('fail'));
 }
 
 module.exports = getPaymentTokenFromAPI;
--- unittests_in_js/6-payment_token.test.js
+++ unittests_in_js/6-payment_token.test.js
@@ -2,9 +2,12 @@ const getPaymentTokenFromAPI = require('./6-payment_token')
 
 describe("getPaymentTokenFromAPI", function () {
   it("should get the result of getPaymentTokenFromAPI", function (done) {
-    getPaymentTokenFromAPI(true)
+    getPaymentTokenFromAPI(false)
     .then(() => {
       done();
+    },
+    (err) => {
+      done(err);
     })
   });
 })
