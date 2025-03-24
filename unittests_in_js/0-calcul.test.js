c843ae58183f4132a04eded3e5f13d84914dba2a
VandevilleF <flo.vandeville@gmail.com>
2025-03-24 11:57:22 +0100

    Task 0, local test OK

---
+++ unittests_in_js/0-calcul.test.js
@@ -0,0 +1,23 @@
+const calculateNumber = require("./0-calcul")
+const assert = require('assert');
+
+describe("calculateNumber", function() {
+  it("Round a and b and return the sum of it", function() {
+    assert.equal(calculateNumber(1, 3), 4);
+  });
+});
+describe("calculateNumber", function() {
+  it("Round a and b and return the sum of it", function() {
+    assert.equal(calculateNumber(1, 3.7), 5);
+  });
+});
+describe("calculateNumber", function() {
+  it("Round a and b and return the sum of it", function() {
+    assert.equal(calculateNumber(1.2, 3.7), 5);
+  });
+});
+describe("calculateNumber", function() {
+  it("Round a and b and return the sum of it", function() {
+    assert.equal(calculateNumber(1.5, 3.7), 6);
+  });
+});
