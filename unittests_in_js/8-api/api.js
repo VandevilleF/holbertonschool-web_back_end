f1c8631e35e3dc81f8fa71e909a43fea340dc974
VandevilleF <flo.vandeville@gmail.com>
2025-03-25 12:06:58 +0100

    split test into two it

--- unittests_in_js/8-api/api.js
+++ unittests_in_js/8-api/api.js
@@ -5,11 +5,11 @@ const port = 7865
 
 app.get('/', (req, res) => {
   res.status(200).send('Welcome to the payment system')
-})
+});
 
 
 app.listen(port, () => {
   console.log(`API available on localhost port ${port}`)
-})
+});
 
 module.exports = app;
--- unittests_in_js/8-api/api.test.js
+++ unittests_in_js/8-api/api.test.js
@@ -6,6 +6,12 @@ describe('api',  () => {
   it('respond with the message Welcome to the payment system', function (done) {
     request.get('http://localhost:7865', (err, res, body) => {
       expect(res.statusCode).to.equal(200);
+      done();
+    })
+  });
+
+  it('respond with the message Welcome to the payment system', function (done) {
+    request.get('http://localhost:7865', (err, res, body) => {
       expect(body).to.equal('Welcome to the payment system');
       done();
     })
