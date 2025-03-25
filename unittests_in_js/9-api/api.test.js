e20573cb10b4eb1c66b2ad3d3e8d9f8dd32d8909
VandevilleF <flo.vandeville@gmail.com>
2025-03-25 20:19:05 +0100

    Split in 2 describes and encapsule it into an other describe

--- unittests_in_js/9-api/api.test.js
+++ unittests_in_js/9-api/api.test.js
@@ -5,7 +5,7 @@ const app = require('./api');
 let server;
 const baseURL = 'http://localhost:7865';
 
-describe('API GET /', () => {
+describe('API', () => {
   before(function (done) {
     server = app.listen(7865, done);
   });
@@ -14,38 +14,42 @@ describe('API GET /', () => {
     server.close(done);
   });
 
-  it('respond with the message Welcome to the payment system', function (done) {
-    request.get(`${baseURL}/`, (err, res, body) => {
-      expect(res.statusCode).to.equal(200);
-      done();
-    })
-  });
-
-  it('respond with the message Welcome to the payment system', function (done) {
-    request.get(`${baseURL}/`, (err, res, body) => {
-      expect(body).to.equal('Welcome to the payment system');
-      done();
-    })
-  });
-  it('responds with 200 for a valid cart ID', function (done) {
-    request.get(`${baseURL}/cart/12`, (err, res, body) => {
-      expect(res.statusCode).to.equal(200);
-      done();
+  describe('API GET /', () => {
+    it('respond with the message Welcome to the payment system', function (done) {
+      request.get(`${baseURL}/`, (err, res, body) => {
+        expect(res.statusCode).to.equal(200);
+        done();
+      });
+    });
+    it('respond with the message Welcome to the payment system', function (done) {
+      request.get(`${baseURL}/`, (err, res, body) => {
+        expect(body).to.equal('Welcome to the payment system');
+        done();
+      });
     });
   });
-
-  it('responds with 200 for a valid cart ID', function (done) {
-    request.get(`${baseURL}/cart/12`, (err, res, body) => {
-      expect(body).to.equal('Payment methods for cart 12');
-      done();
+  describe('API GET /cart/:id', () => {
+    it('responds with 200 for a valid cart ID', function (done) {
+      request.get(`${baseURL}/cart/12`, (err, res, body) => {
+        expect(res.statusCode).to.equal(200);
+        done();
+      });
     });
-  });
 
-  it('responds with 404 for an invalid cart ID not an number', function (done) {
-    request.get(`${baseURL}/cart/hello`, (err, res, body) => {
-      expect(res.statusCode).to.equal(404);
-      done();
+    it('responds with 200 for a valid cart ID', function (done) {
+      request.get(`${baseURL}/cart/12`, (err, res, body) => {
+        expect(body).to.equal('Payment methods for cart 12');
+        done();
+      });
+    });
+
+    it('responds with 404 for an invalid cart ID not an number', function (done) {
+      request.get(`${baseURL}/cart/hello`, (err, res, body) => {
+        expect(res.statusCode).to.equal(404);
+        done();
+      });
     });
   });
 });
+
 
